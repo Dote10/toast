11833d48c9655ce86497a7d903814e8e103aa5ae
surge100 <surge100@statnco.com>
2023-11-21 08:14:56 +0900

    Initial commit

---
+++ src/test/java/com/jam/toast/ToastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jam.toast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
